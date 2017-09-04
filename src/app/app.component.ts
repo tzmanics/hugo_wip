1de14968cc278373498421ed46662a847ce0b0e0
tzmanics <tzmanics@gmail.com>
2017-09-03 21:34:13 -0400

    add test buttons & subscribe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+  hueApi: string = "http://192.168.0.103/api";
+  lights: {};
+
+  constructor(private http: HttpClient) {}
+
+  subscribe() {
+    this.http.post(this.hueApi, { "devicetype":"u_go_hue_go#web tzmanics"})
+      .subscribe(res => console.log(res));
+  }
+
+  getAllLights() {
+    console.log("GET");
+    let url = `${this.hueApi}/lights`;
+    this.http.get(url).subscribe(res => {
+      this.lights = res;
+      console.log("lights" + this.lights);
+    });
+  }
+
+  ngOnInit(): void {}
 }
