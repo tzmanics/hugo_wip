5353df4289b04ce6d463e5fbd812a2f5ca779e26
tzmanics <tzmanics@gmail.com>
2017-09-04 21:33:24 -0400

    GET all lights and ngFor them

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"]
 })
+
 export class AppComponent implements OnInit {
-  hueApi: string = "http://192.168.0.103/api";
-  lights: {};
+  username: string = "<insert username>";
+  hueApi: string = `http://192.168.0.103/api/${this.username}`;
+  lights: string[];
 
   constructor(private http: HttpClient) {}
 
-  subscribe() {
-    this.http.post(this.hueApi, { "devicetype":"u_go_hue_go#web tzmanics"})
-      .subscribe(res => console.log(res));
-  }
+  lightOn(){}
+  lightOff(){}
 
-  getAllLights() {
-    console.log("GET");
-    let url = `${this.hueApi}/lights`;
-    this.http.get(url).subscribe(res => {
-      this.lights = res;
-      console.log("lights" + this.lights);
+  ngOnInit(): void {
+    this.http.get(`${this.hueApi}/lights`)
+    .subscribe(res => {
+      this.lights = Object.keys(res).map(key => res[key]);
+      console.log(this.lights);
     });
   }
-
-  ngOnInit(): void {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
@@ -11,7 +10,6 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     HttpClientModule
   ],
   providers: [],
